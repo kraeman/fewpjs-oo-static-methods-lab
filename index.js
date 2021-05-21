e62a3e36836b4b35261931f20b51a3bd41228291
Krae <kraewind@gmail.com>
2021-05-21 15:11:31 -0400

    commit tree 7106bb3680393cff635c787e15662f6e1929fb27

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    let thing = string.split('')
+    thing[0] = thing[0].toUpperCase()
+    return thing.join('')
+  }
+
+  static sanitize(string){
+    let desiredArray = string.match(/[\w- ']/g)
+    let array = []
+    for (const key in desiredArray) {
+      array.push(desiredArray[key])
+    }
+    return array.join("")
+  }
+
+  static titleize(string) {
+    let empty = []
+    let arrayOfWords = string.split(' ')
+    arrayOfWords.each(word => {
+      const wanted = word.charAt(0).toUpperCase() + word.slice(1)
+      empty.push(wanted)
+    })
+    return empty.join()
+  }
 }
